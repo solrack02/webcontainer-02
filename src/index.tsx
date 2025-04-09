da52568866331d628a0222e10705609de06596e3
Carlos A. <carlosvsk@gmail.com>
2025-04-09 15:24:19 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -44,7 +44,7 @@
           (...args:any) => <Elements.DynView pass={{
             elementsProperties:['{}'],
 
-            styles:[`{ width: 50, height: 50, backgroundColor: "#fff" }`],
+            styles:[`{ width: 50, height: 50, backgroundColor: "yellow" }`],
 
             functions:[()=>{}],            childrenItems:[() =><></>],
 
